a17f4c525e4c79bb8435dc9726c9db6f3eed098c
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-09-10 08:45:21 +0530

    Add files via upload

---
+++ LambdaExpression/src/LambdaExpression.java
@@ -0,0 +1,10 @@
+@FunctionalInterface
+interface Greeting {
+    void sayHello();
+}
+public class LambdaExpression {
+    public static void main(String[] args) {
+        Greeting greeting = ()-> System.out.println("Hello World");
+        greeting.sayHello();
+    }
+}
