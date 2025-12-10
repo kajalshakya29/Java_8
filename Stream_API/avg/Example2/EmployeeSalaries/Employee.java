09b6e015500a13824ecb29fe914850a3cafb8d88
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-12-11 01:11:57 +0530

    Add files via upload

---
+++ Stream_API/avg/Example2/EmployeeSalaries/Employee.java
@@ -0,0 +1,10 @@
+package streams.avg.Example2.EmployeeSalaries;
+class Employee {
+    String name;
+    int salary;
+
+    Employee(String name, int salary){
+        this.name = name;
+        this.salary = salary;
+    }
+}
---
+++ Stream_API/avg/Example3/StudentsMarks/Student.java
@@ -0,0 +1,10 @@
+package streams.avg.Example3.StudentsMarks;
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks){
+        this.name = name;
+        this.marks = marks;
+    }
+}
