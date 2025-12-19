7a5a166480f560579672270eca51d3417f63b7a1
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-12-19 14:16:43 +0530

    Add files via upload

---
+++ Biconsumer/src/Example4_EmployeeNameSalaryPrint/Employee.java
@@ -0,0 +1,10 @@
+package functional_interface_with_lambda.BiConsumer.Example4_EmployeeNameSalaryPrint;
+class Employee {
+    String name;
+    int salary;
+
+    Employee(String name, int salary){
+        this.name = name;
+        this.salary = salary;
+    }
+}
